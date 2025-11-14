44c5b065d1ad8e0b3ca04e5a052bf689a6c5b4d1
Rádi Olivér <radi.oliver@diak.szbi-pg.hu>
2025-11-14 09:47:02 +0100

    ad

--- age_test/age.py
+++ age_test/age.py
@@ -1,13 +1,12 @@
 def test_age(age):
-    if  0 <= age < 9:
+    if  0 <= age <= 9:
         return "child"
-    elif 9 <= age < 18:
+    elif 9 < age < 18:
         return "teenager"
-    elif 18 <= age < 65:
+    elif 18 <= age <= 65:
         return "adult"
-    elif 65 <= age < 125:
+    elif 65 < age <= 125:
         return "golden age"
     else:
         return f"{age} is not a valid age"
-    
     
--- age_test/test_age.py
+++ age_test/test_age.py
@@ -1,14 +1,14 @@
-import pytest
-from age import age
+import unittest
+from age import test_age
 
-def test_age(age):
-    if  0 <= age < 9:
-        return "child"
-    elif 9 <= age < 18:
-        return "teenager"
-    elif 18 <= age < 65:
-        return "adult"
-    elif 65 <= age < 125:
-        return "golden age"
-    else:
-        return f"{age} is not a valid age"
+class TestAge(unittest.TestCase):
+    def test_age(self):
+        self.assertEqual(test_age(0), "child")
+        self.assertEqual(test_age(9), "child")
+        self.assertEqual(test_age(10), "teenager")
+        self.assertEqual(test_age(17), "teenager")
+        self.assertEqual(test_age(18), "adult")
+        self.assertEqual(test_age(65), "adult")
+        self.assertEqual(test_age(66), "golden age")
+        self.assertEqual(test_age(125), "golden age")
+        self.assertEqual(test_age(126), "126 is not a valid age")
